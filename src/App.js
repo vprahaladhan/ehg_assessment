119fc4b1fdc7490de5166b7548ef3d756054fb81
prahal <prahal@users.noreply.github.com>
2021-10-09 23:36:23 +1100

    commit before trying state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+// import ReactTooltip from 'react-tooltip';
+import "semantic-ui-css/semantic.min.css";
+import { Popup } from "semantic-ui-react";
+
 import './App.css';
 
-function App() {
+// const colorToHex = color => {
+//   var hex = color.toString(16);
+//   return (hex.length === 1 && "0" + hex) || hex;
+// }
+
+// const colorToHex = color => color.toString(16).padStart(2, '0');
+
+// const convertRGBtoHex = (red, green, blue) => {
+//   return '#' + colorToHex(red) + colorToHex(green) + colorToHex(blue);
+// }
+
+const App = () => {
+  var colorBlocks = [];
+
+  for (var red = 8; red < 256; red += (red === 248) ? 7 : 8) {
+    const redInHEX = red.toString(16).padStart(2, '0');
+
+    for (var green = 8; green < 256; green += (green === 248) ? 7 : 8) {
+      const greenInHEX = green.toString(16).padStart(2, '0');
+
+      for (var blue = 8; blue < 256; blue += (blue === 248) ? 7 : 8) {
+        const blueInHEX = blue.toString(16).padStart(2, '0');
+        const backgroundColor = `#${redInHEX}${greenInHEX}${blueInHEX}`;
+
+        colorBlocks.push(
+          <Popup
+            key={backgroundColor}
+            trigger={<div className="inner" style={{ backgroundColor }}></div>}
+          >
+            {backgroundColor}
+          </Popup>
+        );
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      {colorBlocks}
     </div>
   );
 }
